59de245b0ed6fd4de761a5de977782e1ebce1eff
Enno Boland <g@s01.de>
2015-05-22 09:46:17 +0200

    rebuild gh-pages based on ba7f2a0fa584c5c96f14f33fa4e79c7b5b21a29b

--- HelloWorld.java
+++ HelloWorld.java
@@ -7,7 +7,20 @@
 public class HelloWorld
 {
 	public HelloWorld() {
-		hw = foo;
+	}
+
+	public static void main(String[] argv) {
+		HelloWorld hw = new HelloWorld();
+		hw.printHello();
+		hw.printWorld();
+	}
+
+	public void printHello() {
+		System.out.println("Hello");
+	}
+
+	public void printWorld() {
+		System.out.println("World");
 	}
 }
 
