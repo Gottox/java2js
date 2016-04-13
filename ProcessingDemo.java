af0133ad157e2d42e96ba1afee69453f3e148ba4
Enno Boland <g@s01.de>
2016-04-13 08:25:51 +0200

    rebuild gh-pages based on 725260ce6389791bfd4d0cd48f6247876df1a417

--- ProcessingDemo.java
+++ ProcessingDemo.java
@@ -4,50 +4,23 @@
  * Example file
  */
 
-class ProcessingDemo extends processing{
+class ProcessingDemo {
 	// Global variables
 	float radius = 50.0f;
 	int X, Y;
 	int nX, nY;
 	int delay = 16;
 
+	public ProcessingDemo(Processing processing) {
+		this.processing = processing;
+	}
+
 	// Setup the Processing Canvas
 	public void setup(){
-		this.size(200, 200);
-		this.strokeWeight( 10 );
-		this.frameRate(15);
-		this.X = width / 2;
-		this.Y = width / 2;
-		this.nX = this.X;
-		this.nY = this.Y;
 	}
 
 	// Main draw loop
 	public void draw(){
-
-		this.radius = this.radius + sin( this.frameCount / 4 );
-
-		// Track circle to new destination
-		this.X+=(this.nX-this.X)/this.delay;
-		this.Y+=(this.nY-this.Y)/this.delay;
-
-		// Fill canvas grey
-		this.background( 100 );
-
-		// Set fill-color to blue
-		this.fill( 0, 121, 184 );
-
-		// Set stroke-color white
-		this.stroke(255);
-
-		// Draw circle
-		this.ellipse( this.X, this.Y, this.radius, this.radius );
-	}
-
-
-	// Set circle's next destination
-	void mouseMoved(){
-		this.nX = this.mouseX;
-		this.nY = this.mouseY;
+		this.processing.background(224);
 	}
 }
